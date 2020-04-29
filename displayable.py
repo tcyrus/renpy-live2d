b9fab1e3885b7b5a85d2a6c3e2ad55161935bd69
Timothy Cyrus <4201229+tcyrus@users.noreply.github.com>
2020-04-29 19:56:59 -0400

    Update displayable.py

--- displayable.py
+++ displayable.py
@@ -7,22 +7,22 @@ from renpy.display.core import Interface
 
 # TODO: Do not forget to call 'PyCubismFramework.dispose()'.
 PyLAppDelegate.initialize()
-    
+
 class Live2DDisplayable(renpy.Displayable):
-        
+
     def __init__(self, **kwargs):
         super(Live2DDisplayable, self).__init__(**kwargs)
-    
+
         self.scene = PyLAppScene()
         self.scene_initialized = False
-        
+
         self.render_width = 1024
         self.render_height = 512
-                    
+
         interface_set_mode_old = Interface.set_mode
 
         self_displayable = self
-        
+
         def interface_set_mode_new(self, physical_size=None):
             interface_set_mode_old(self, physical_size)
             self_displayable.initialize()
@@ -31,19 +31,19 @@ class Live2DDisplayable(renpy.Displayable):
 
     def initialize(self):
         self.scene.initialize(self.render_width, self.render_height)
-        
-    def render(self, width, height, st, at):                 
+
+    def render(self, width, height, st, at):
         self.scene.update()
-                                  
+
         result = renpy.Render(self.render_width, self.render_height)
         surface = result.canvas().get_surface()
         surface.lock()
-        self.scene.draw(surface.get_pitch() // surface.get_bytesize(), surface._pixels_address)        
+        self.scene.draw(surface.get_pitch() // surface.get_bytesize(), surface._pixels_address)
         surface.unlock()
-                                    
+
         renpy.display.render.redraw(self, 0.06)
-                
+
         return result
-        
+
     def create_model(self, model_dir, model_file):
-        return self.scene.create_model(model_dir, model_file)   
+        return self.scene.create_model(model_dir, model_file)
